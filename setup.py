558696d9b0371c27c0b5e8900c801254bebbb514
EtienneCmb <e.combrisson@gmail.com>
2016-12-09 16:53:01 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='pysoroban',
-        version='0.0',
+        version='0.1',
         packages=['pysoroban'],
         description='Simple program for soroban training',
         install_requires=[
