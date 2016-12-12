d050d63430dfeecdc70e09ef4b075ed660fa6d98
EtienneCmb <e.combrisson@gmail.com>
2016-12-12 15:37:34 +0100

    Fix ,

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='pysoroban',
-        version='0.2',
+        version='0.21',
         packages=['pysoroban'],
         description='Simple program for soroban training',
         install_requires=[
